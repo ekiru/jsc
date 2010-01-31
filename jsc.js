6fd41408a7ce3bce104b268983f93e702425affa
Tyler Leslie Curtis <ekiru.0@gmail.com>
2010-01-31 14:38:50 -0600

    Added scope handling. Note that defining nested functions will not work properly.

--- jsc.js
+++ jsc.js
@@ -1,201 +1,213 @@
 /*
-Copyright (c) 2010 Tyler Leslie Curtis <ekiru.0@gmail.com>
+  Copyright (c) 2010 Tyler Leslie Curtis <ekiru.0@gmail.com>
 
- Permission is hereby granted, free of charge, to any person
- obtaining a copy of this software and associated documentation
- files (the "Software"), to deal in the Software without
- restriction, including without limitation the rights to use,
- copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the
- Software is furnished to do so, subject to the following
- conditions:
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation
+  files (the "Software"), to deal in the Software without
+  restriction, including without limitation the rights to use,
+  copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following
+  conditions:
 
- The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software.
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- OTHER DEALINGS IN THE SOFTWARE.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
 */
 
 importPackage(java.io, java.lang);
 
 load("jsc_config.js");
 load("jsc_modules.js");
+load("jsc_scopes.js");
 load("util.js");
 
 
 function compile (prog) {
-  var symbols_used = [];
-  var functions_defined = {};
-  var main_text = wrapMain(prog, symbols_used, functions_defined);
-
-  var include_text = '#include "js_types.h"\n';
-  var included_files = { "js_types.h" : true };
-  foreach(symbols_used, 
-      function (symbol) {
-	var file = jsc_symbol_locations[symbol];
-	if (file != undefined && included_files[file] == undefined) {
-	  include_text += "#include " + 
-	    "\"" + file + "\"\n";
-	  included_files[file] = true;
-	}
-      });
-
-  var declaration_text = "";
-  var definition_text = "";
-  foreach(functions_defined,
-	  function (func) {
-	    declaration_text += func["declaration"];
-	    definition_text += func["definition"];
-	  });
-
-  return include_text + declaration_text + main_text + definition_text;
+    var symbols_used = [];
+    var functions_defined = {};
+    var globalScope = new Scope(null);
+    var main_text = wrapMain(prog, symbols_used, functions_defined, globalScope);
+    
+    var include_text = '#include "js_types.h"\n';
+    var included_files = { "js_types.h" : true };
+    foreach(symbols_used, 
+	    function (symbol) {
+		var file = jsc_symbol_locations[symbol];
+		if (file != undefined && included_files[file] == undefined) {
+		    include_text += "#include " + 
+			"\"" + file + "\"\n";
+		    included_files[file] = true;
+		}
+	    });
+    
+    var declaration_text = "";
+    var definition_text = "";
+    foreach(functions_defined,
+	    function (func) {
+		declaration_text += func["declaration"];
+		definition_text += func["definition"];
+	    });
+    
+    return include_text + declaration_text + main_text + definition_text;
 }
 
-function wrapMain (main, symbols_used, functions_defined) {
-  var body = compileProgram(main, symbols_used, functions_defined);
-  var resultText = 
-    "int main() {" + "\n" +
-    body +
-    "return 0;" + "\n" +
-    "}\n";
-  return resultText;
+function wrapMain (main, symbols_used, functions_defined, scope) {
+    var body = compileProgram(main, symbols_used, functions_defined, scope);
+    var resultText = 
+	"int main() {" + "\n" +
+	body +
+	"return 0;" + "\n" +
+	"}\n";
+    return resultText;
 }
 
-function compileProgram(prog, symbols_used, functions_defined) {
-  if (!prog) {
-    return "{}\n";
-  } else  if (prog[0] == "do") {
-    return compileDo(prog, symbols_used, functions_defined);
-  } else if (prog[0] == "defun") {
-    compileDefun(prog, symbols_used, functions_defined);
-    return "";
-  } else if (prog[0] == "if") {
-    return compileIf(prog, symbols_used, functions_defined);
-  } else {
-    return compileStatement(prog, symbols_used, functions_defined);
-  }
+function compileProgram(prog, symbols_used, functions_defined, scope) {
+    if (!prog) {
+	return "{}\n";
+    } else  if (prog[0] == "do") {
+	return compileDo(prog, symbols_used, functions_defined, scope);
+    } else if (prog[0] == "defun") {
+	compileDefun(prog, symbols_used, functions_defined, scope);
+	return "";
+    } else if (prog[0] == "if") {
+	return compileIf(prog, symbols_used, functions_defined, scope);
+    } else {
+	return compileStatement(prog, symbols_used, functions_defined, scope);
+    }
 }
 
-function compileDo(prog, symbols_used, functions_defined) {
-  var statements = rest(prog);
-  var resultText = "{\n";
+function compileDo(prog, symbols_used, functions_defined, scope) {
+    var statements = rest(prog);
+    var resultText = "{\n";
 
-  foreach(statements, 
-      function (statement) {
-	var comp = compileProgram(statement,
-				  symbols_used, functions_defined);
-	resultText += comp;
-      });
-  resultText += "}\n";
+    foreach(statements, 
+	    function (statement) {
+		var comp = compileProgram(statement,
+					  symbols_used, functions_defined, scope);
+		resultText += comp;
+	    });
+    resultText += "}\n";
 
-  return resultText;
+    return resultText;
 }
 
-function compileDefun(defun, symbols_used, functions_defined) {
-  var name = moduleFunction("user", defun[1]);
-  if (defined(functions_defined[name])) {
-    throw Error("Multiple definitions of function " + name + ".");
-  }
-  var args = defun[2];
-  var body = defun[3];
-  var signature = 
-    "void " + name +
-    "(" + map(args, function(str) {
-	return "struct js_value *" + str;
-      }).join(", ") + ")";
+function compileDefun(defun, symbols_used, functions_defined, scope) {
+    var internalScope = new Scope(scope);
 
-  var declText = signature + ";\n"
-  var defText = 
-    signature + " {\n" +
-    compileProgram(body, symbols_used, functions_defined) +
-    "}\n";
+    var name = moduleFunction("user", defun[1]);
+    if (defined(functions_defined[name])) {
+	throw Error("Multiple definitions of function " + name + ".");
+    }
+    var args = defun[2];
+    foreach(args, function (arg) {
+	    internalScope.bind(arg);
+	});
+
+    var body = defun[3];
+    var signature = 
+	"void " + name +
+	"(" + map(args, function(str) {
+		return "struct js_value *" + str;
+	    }).join(", ") + ")";
+
+    var declText = signature + ";\n"
+	var defText = 
+	signature + " {\n" +
+	compileProgram(body, symbols_used, functions_defined, internalScope) +
+	"}\n";
   
-  functions_defined[name] = { "declaration" : declText,
-			      "definition" : defText };
+    functions_defined[name] = { "declaration" : declText,
+				"definition" : defText };
 }
 
-function compileIf(cond, symbols_used, functions_defined) {
-  var condition = cond[1];
-  var thenb = cond[2];
-  var elseb = cond[3];
+function compileIf(cond, symbols_used, functions_defined, scope) {
+    var condition = cond[1];
+    var thenb = cond[2];
+    var elseb = cond[3];
 
-  symbols_used.push(jsc_builtins["is_true"]);
-  var text = 
-    ("if (" + jsc_builtins["is_true"] + "(" + 
-     compileExpr(condition, symbols_used, functions_defined) + ")) " +
-     compileProgram(thenb, symbols_used, functions_defined)) +
-    ("else " + compileProgram(elseb, symbols_used, functions_defined));
+    symbols_used.push(jsc_builtins["is_true"]);
+    var text = 
+	("if (" + jsc_builtins["is_true"] + "(" + 
+	 compileExpr(condition, symbols_used, functions_defined, scope) + ")) " +
+	 compileProgram(thenb, symbols_used, functions_defined, scope)) +
+	("else " + compileProgram(elseb, symbols_used, functions_defined, scope));
 
-  return text;
+    return text;
 }
 
-function compileStatement(prog, symbols_used, functions_defined) {
-  return compileExpr(prog, symbols_used, functions_defined) + ";\n";
+function compileStatement(prog, symbols_used, functions_defined, scope) {
+    return compileExpr(prog, symbols_used, functions_defined, scope) + ";\n";
 }
 
-function compileExpr(expr, symbols_used, functions_defined) {
-  switch (expr[0]) {
-  case "call":
-    return compileCall(expr, symbols_used, functions_defined);
-  case "lambda":
-    return compileLambda(expr, symbols_used, functions_defined);
-  case "int":
-    symbols_used.push(jsc_builtins['create_fixnum']);
-    return jsc_builtins['create_fixnum'] + '(' + expr[1].toString() + ')';
-  case "string":
-    symbols_used.push(jsc_builtins['create_string']);
-    return jsc_builtins['create_string'] + '("' + expr[1] + '")';
-  case "varget":
-    return expr[1].toString();
-  default:
-    throw TypeError("Invalid expression type.");
-  }
+function compileExpr(expr, symbols_used, functions_defined, scope) {
+    switch (expr[0]) {
+    case "call":
+	return compileCall(expr, symbols_used, functions_defined, scope);
+    case "lambda":
+	return compileLambda(expr, symbols_used, functions_defined, scope);
+    case "int":
+	symbols_used.push(jsc_builtins['create_fixnum']);
+	return jsc_builtins['create_fixnum'] + '(' + expr[1].toString() + ')';
+    case "string":
+	symbols_used.push(jsc_builtins['create_string']);
+	return jsc_builtins['create_string'] + '("' + expr[1] + '")';
+    case "varget":
+	if (scope.binds(expr[1])) {
+	    return expr[1].toString();
+	} else {
+	    throw Error("Variable " + expr[1] + " not bound.");
+	}
+    default:
+	throw TypeError("Invalid expression type.");
+    }
 }
 
-function compileCall(expr, symbols_used, functions_defined) {
-var resultString = "";
- var func = expr[1];
- var args = rest(rest(expr));
- if (Array.isArray(func)) {
-   func = compileExpr(func, symbols_used, functions_defined);
- }
+function compileCall(expr, symbols_used, functions_defined, scope) {
+    var resultString = "";
+    var func = expr[1];
+    var args = rest(rest(expr));
+    if (Array.isArray(func)) {
+	func = compileExpr(func, symbols_used, functions_defined, scope);
+    }
 
- if (jsc_builtins[func] !== undefined) {
-   resultString += jsc_builtins[func];
-   symbols_used.push(jsc_builtins[func]);
- } else {
-   resultString += moduleFunction("user", func);
- }
- resultString += "(";
+    if (jsc_builtins[func] !== undefined) {
+	resultString += jsc_builtins[func];
+	symbols_used.push(jsc_builtins[func]);
+    } else {
+	resultString += moduleFunction("user", func);
+    }
+    resultString += "(";
  
- if (args.length > 0) {
-   foreach(args, 
-	   function (arg) {
-	     var comp = compileExpr(arg, symbols_used, functions_defined);
-	     resultString += comp + ",";
-	   });
-   resultString = resultString.substring(0, resultString.length - 1);
- }
- resultString += ")";
+    if (args.length > 0) {
+	foreach(args, 
+		function (arg) {
+		    var comp = compileExpr(arg, symbols_used, functions_defined, scope);
+		    resultString += comp + ",";
+		});
+	resultString = resultString.substring(0, resultString.length - 1);
+    }
+    resultString += ")";
  
- return resultString;
+    return resultString;
 }
 
 var lambda_count = 0;
-function compileLambda(lambda, symbols_used, functions_defined) {
-  var name = "lambda_" + lambda_count;
-  lambda_count++;
-  compileDefun(["defun", name, lambda[1],
-		lambda[2]], symbols_used, functions_defined);
+function compileLambda(lambda, symbols_used, functions_defined, scope) {
+    var name = "lambda_" + lambda_count;
+    lambda_count++;
+    compileDefun(["defun", name, lambda[1],
+		  lambda[2]], symbols_used, functions_defined, scope);
 
-  return name;
+    return name;
 }
 
 hello_world = ["call", "println", ["string", "Hello, world!"]];
@@ -208,7 +220,7 @@ multiple_exprs = ["do",
 subexprs = ["do", 
 	    ["call", "print", ["string", "'Hello world' takes "]],
 	    ["call", "print", ["call", "strlen", 
-				   ["string", "Hello world"]]],
+			       ["string", "Hello world"]]],
 	    ["call", "println", ["string", "bytes."]]];
 
 defun = ["do",
